ad67223b94f8a261a3890456a1342cb3270da6e0
Esen Espinosa <sujumayas@gmail.com>
2019-07-15 04:25:17 -0500

    changed remote server config and docker hub config

--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo maven-project > /tmp/.auth
+echo maven-automated-pipeline > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth prod-user@linuxfacilito.online:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod-user@linuxfacilito.online:/tmp/publish
-ssh -i /opt/prod prod-user@linuxfacilito.online "/tmp/publish"
+scp -i /opt/prod /tmp/.auth prod-user@ec2-54-175-13-162.compute-1.amazonaws.com:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish prod-user@ec2-54-175-13-162.compute-1.amazonaws.com:/tmp/publish
+ssh -i /opt/prod prod-user@ec2-54-175-13-162.compute-1.amazonaws.com "/tmp/publish"
--- pipeline/jenkins/push/push.sh
+++ pipeline/jenkins/push/push.sh
@@ -4,11 +4,11 @@ echo "********************"
 echo "** Pushing image ***"
 echo "********************"
 
-IMAGE="maven-project"
+IMAGE="maven-automated-pipeline"
 
 echo "** Logging in ***"
-docker login -u ricardoandre97 -p $PASS
+docker login -u sujumayas -p $PASS
 echo "*** Tagging image ***"
-docker tag $IMAGE:$BUILD_TAG ricardoandre97/$IMAGE:$BUILD_TAG
+docker tag $IMAGE:$BUILD_TAG sujumayas/$IMAGE:$BUILD_TAG
 echo "*** Pushing image ***"
-docker push ricardoandre97/$IMAGE:$BUILD_TAG
+docker push sujumayas/$IMAGE:$BUILD_TAG
