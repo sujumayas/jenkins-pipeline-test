02a29a88300c9c1004fe978b4643e89124b99dc3
Esen Espinosa <sujumayas@gmail.com>
2019-07-15 02:29:51 -0500

    changed the directory in workspace in maven script

--- pipeline/jenkins/build/mvn.sh
+++ pipeline/jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/home/ec2-user/jenkins-pipeline-test/jenkins_home/workspace/pipeline-docker-maven
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
