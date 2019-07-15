c5b4c5fdb9fd697ab4365860cf8a3b5582783a29
Esen Espinosa <sujumayas@gmail.com>
2019-07-15 18:12:57 -0500

    forgot to save file.... uhm...

--- pipeline/jenkins/build/mvn.sh
+++ pipeline/jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE=/home/ec2-user/jenkins-pipeline-test/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/home/ec2-user/jenkins-pipeline-test/jenkins_home/workspace/pipeline-docker-maven/pipeline
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
