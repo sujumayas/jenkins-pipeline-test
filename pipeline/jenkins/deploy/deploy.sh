28c4c23534a4ee636aa8c39d76800b41c7dc1b4a
Esen Espinosa <sujumayas@gmail.com>
2019-07-15 18:05:34 -0500

    Changed directory structure

--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -5,5 +5,5 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 scp -i /opt/prod /tmp/.auth prod-user@ec2-54-175-13-162.compute-1.amazonaws.com:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod-user@ec2-54-175-13-162.compute-1.amazonaws.com:/tmp/publish
+scp -i /opt/prod ./pipeline/jenkins/deploy/publish prod-user@ec2-54-175-13-162.compute-1.amazonaws.com:/tmp/publish
 ssh -i /opt/prod prod-user@ec2-54-175-13-162.compute-1.amazonaws.com "/tmp/publish"
