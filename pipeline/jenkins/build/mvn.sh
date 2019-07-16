6615195a5818405f992945588c7cbb0b735361ad
Esen Espinosa <sujumayas@gmail.com>
2019-07-15 21:19:26 -0500

    Changed the veersion of docker image of maven to test

--- pipeline/jenkins/build/mvn.sh
+++ pipeline/jenkins/build/mvn.sh
@@ -6,6 +6,6 @@ echo "***************************"
 
 WORKSPACE=/home/ec2-user/jenkins-pipeline-test/jenkins_home/workspace/pipeline-docker-maven/pipeline
 
-docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.3-jdk-8 "$@"
 
 
