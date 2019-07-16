bd1f133ff9f49716d65a8e8cd071120852e2d45c
Esen Espinosa <sujumayas@gmail.com>
2019-07-15 21:58:32 -0500

    going back to simple application tests

--- pipeline/java-app/src/test/java/com/sujumayas/clientsmicroservice/SomeTest.java
+++ pipeline/java-app/src/test/java/com/sujumayas/clientsmicroservice/SomeTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SomeTest {
 
 	@Test
-	public void contextLoads() {
+	public void contexLoads() throws Exception {
 		
 	}
 
